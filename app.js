ca75b172052ed1745ccf833161b7f0f51ec03117
pratikkadam00 <pratikadam00@gmail.com>
2024-09-14 20:27:26 +0530

    updated

--- app.js
+++ app.js
@@ -8,16 +8,15 @@ import helmet from "helmet";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
+dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: 'https://hackathon-frontend-two.vercel.app', // Specify the allowed origin
-  credentials: true // If you need to allow credentials
+  origin: 'https://hackathon-frontend-two.vercel.app', 
+  credentials: true 
 }));
 
-dotenv.config();
-app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
