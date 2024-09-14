8d398fc5218cf982c0d56dc6e60db55106b42fe4
pratikkadam00 <pratikadam00@gmail.com>
2024-09-14 13:39:59 +0530

    crud added in events

--- route/eventRoutes.js
+++ route/eventRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { createEvent, getEvents } from "../controllers/eventController.js";
+import {
+    createEvent,
+    getEvents,
+    getEventById,
+    updateEvent,
+    deleteEvent,
+} from "../controllers/eventController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createEvent);
 router.get("/", protect, getEvents);
+router.get("/:id", protect, getEventById);
+router.put("/:id", protect, updateEvent);
+router.delete("/:id", protect, deleteEvent);
 
 export default router;
