dc198c31836ebd4233910fad2bbbec7d02330d30
Pratikkadam00 <pratikadam00@gmail.com>
2024-09-14 15:01:38 +0530

    token removed from auth register api and role added

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,19 +3,20 @@ import generateToken from "../utils/jwtUtils.js";
 
 // Register User
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
 
   const userExists = await User.findOne({ email });
   if (userExists) {
     return res.status(400).json({ message: "User already exists" });
   }
 
-  const user = await User.create({ name, email, password });
+  const user = await User.create({ name, email, password, role });
   res.status(201).json({
     _id: user._id,
     name: user.name,
+    role: user.role,
     email: user.email,
-    token: generateToken(user._id),
+    // token: generateToken(user._id),
   });
 };
 
@@ -25,14 +26,12 @@ const loginUser = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
-    })
-    ;
+    });
   } else {
     res.status(401).json({ message: "Invalid email or password" });
   }
